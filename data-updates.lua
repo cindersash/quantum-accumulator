c2a4bede3821059e62885e73a4b558fc7af9e388
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2025-04-10 20:54:08 -0500

    Version 1.0.1

--- data-updates.lua
+++ data-updates.lua
@@ -2,3 +2,5 @@ table.insert(data.raw["technology"]["quantum-processor"].effects, {
   type = "unlock-recipe",
   recipe = "qa_quantum-accumulator"
 })
+
+data.raw["accumulator"]["accumulator"].next_upgrade = "qa_quantum-accumulator"
